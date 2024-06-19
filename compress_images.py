0295c7d6415a4590e2c46c15a61a012736bce479
jExbrayat <juexbrayat@hop.fr>
2024-06-19 09:26:59 +0200

    refactor: make script to write compressed images as bytes file

--- compress_images.py
+++ compress_images.py
@@ -1,18 +1,37 @@
 import json
 import numpy as np
-
 from PIL import Image
+import io
 
 # Load meshgrid
 with open("assets/decoded_meshgrid.json", "r") as file:
     decoded_meshgrid = json.load(file)
 
-for img_idx in range(len(decoded_meshgrid)):  # Iterate over images of shape (784,)
+# Create a list to store image bytes
+image_bytes_list = []
 
+for img_idx in range(len(decoded_meshgrid)):  # Iterate over images of shape (784,)
     img = np.array(decoded_meshgrid[img_idx])  # Convert as np array
     img = img.reshape(28, 28)
 
     pil_img = Image.fromarray(img)  # Convert as pillow object
     compressed_img = pil_img.convert("L")  # Convert to 8-bit pixels, grayscale
 
-    compressed_img.save(f"imgs/decoded_meshgrid_{img_idx}.png")
+    # Save image bytes to list
+    with io.BytesIO() as output:
+        compressed_img.save(output, format="PNG")
+        img_bytes = output.getvalue()
+        image_bytes_list.append(img_bytes)
+
+
+# Write all image bytes to a binary file
+with open("assets/decoded_meshgrid.bin", "wb") as binary_file:
+    for img_bytes in image_bytes_list:
+        binary_file.write(img_bytes)
+
+# Here is how bytes sequences of different images are separated
+for img in image_bytes_list[:10]:
+    header = [byte for byte in img[:4]]
+    converted_header = bytes(header)
+    print(header)
+    print(converted_header)
